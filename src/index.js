7b8999315801ac82bf91aed3a8f541a51bd5b167
Shashant Shekhar <shashantshekhar10@gmail.com>
2024-07-05 19:04:07 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-const express = require('express')
-require('dotenv').config()
-const bodyParser = require("body-parser")
+const express = require("express");
+// require("dotenv").config();
+const bodyParser = require("body-parser");
 
-const { PORT } = require('./config/serverConfig')
+const { PORT } = require("./config/serverConfig");
+const CityRepository = require('./repository/city-repository')
 
 const setupAndStartServer = async () => {
-          const app = express()
+  const app = express();
 
-          app.use(bodyParser.json())
-          app.use(bodyParser.urlencoded({extended: true}))
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-          app.listen(PORT, () => {
-                    console.log(`server started at ${PORT}`);
-          })
-}
+  app.listen(PORT, () => {
+    console.log(`server started at ${PORT}`);
+  });
+};
 
-setupAndStartServer()
+setupAndStartServer();
 
 // "test": "echo \"Error: no test specified\" && exit 1"
