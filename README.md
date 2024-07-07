f95b51a201931816a69f2141ed78a19e53027f54
Shashant Shekhar <shashantshekhar10@gmail.com>
2024-07-08 01:20:38 +0530

    add airports model

--- README.md
+++ README.md
@@ -11,3 +11,9 @@
     services/
 -tests/ [later]
 -static/
+
+## Tables
+
+### City => id, name,created_at,updated_at
+### Airport => id,name,address, city_id, created_at, updated_at
+      Realationship -> City has many airports and Airport belongs to a city (one to many)
--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,4 +1,6 @@
 const { where } = require("sequelize");
+const { Op } = require("sequelize");
+
 const { City } = require("../models/index");
 
 class CityRepository {
@@ -59,8 +61,19 @@ class CityRepository {
     }
   }
 
-  async getAllCities() {
+  async getAllCities(filter) {
     try {
+      if (filter.name) {
+        const cities = await City.findAll({
+          where: {
+            name: {
+              [Op.startsWith]: filter.name,
+            },
+          },
+        });
+        return cities;
+      }
+
       const cities = await City.findAll();
       return cities;
     } catch (error) {
